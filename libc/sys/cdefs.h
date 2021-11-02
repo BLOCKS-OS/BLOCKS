33b12a608130a9ca85688b912f8fdb61ce9dac43
Abhishek Srivastava <abhisheksrivastavabbn@gmail.com>
2021-11-02 13:40:01 +0530

    changes

--- libc/sys/cdefs.h
+++
@@ -1,9 +0,0 @@
-/* Declares a bunch of useful preprocessor macros meant for 
-internal use by the standard library */
-
-#ifndef _SYS_CDEFS_H
-#define _SYS_CDEFS_H 1
- 
-#define __blocks_libc 1
- 
-#endif
